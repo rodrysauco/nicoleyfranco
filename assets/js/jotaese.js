b387892b08271ba2fd01cf115043434225727e67
Rodrigo Sauco <rodry.sauco@gmail.com>
2025-07-13 22:59:17 +0200

    first commit

---
+++ assets/js/jotaese.js
@@ -0,0 +1,39 @@
+const startDate = new Date("2025-10-25T21:00:00");
+const finishDate = new Date("2025-10-26T05:30:00");
+const countdownEl = document.getElementById("countdown");
+
+const timer = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = startDate - now;
+  const hasFinished = finishDate - now;
+  if (distance < 0) {
+    clearInterval(timer);
+    countdownEl.textContent = "¡La fiesta ya empezo!";
+    return;
+  }
+
+  if (hasFinished < 0) {
+    clearInterval(timer);
+    countdownEl.textContent = "La fiesta termino :(";
+    return;
+  }
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  countdownEl.textContent = `¡Faltan ${days} dias, ${hours} horas y ${minutes} minutos!`;
+}, 1000);
+
+const params = new URLSearchParams(window.location.search);
+const nombre = params.get("nombre");
+
+document.getElementById("mensaje-invitado").textContent = `¿${nombre} venis?`;
+
+const formURL = `https://docs.google.com/forms/d/e/1FAIpQLSexu-MnsBi1IpPapimaUHc89SV_4XnXUAS8XNhWhoxEJDK2tA/viewform?usp=pp_url&entry.1044132097=${encodeURIComponent(
+  nombre
+)}`;
+document.getElementById("google-form-link").href = formURL;
